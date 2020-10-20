bb071de17848b98f3fa0f8181d4b6ed6c72b3342
hyperchessbot <hyperchessbot@gmail.com>
2020-10-20 11:44:39 +0000

    Chat message

--- server.js
+++ server.js
@@ -139,7 +139,7 @@ app.get('/', (req, res) => {
 function playGame(gameId){    
     console.log(`playing game: ${gameId}`)
 
-    gameChat(gameId, "all", `${lichessBotName} running on code https://github.com/hyperchessbot/hyperbot`)
+    gameChat(gameId, "all", `${lichessBotName} running on https://github.com/hyperchessbot/hyperbot`)
     gameChat(gameId, "all", `Good luck !`)
 
     playingGameId = gameId
