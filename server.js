95ca41a26258ea75bfb924eb4a549cff84d179e2
hyperchessbot <hyperchessbot@gmail.com>
2020-10-23 13:42:06 +0000

    Readme

--- server.js
+++ server.js
@@ -232,6 +232,13 @@ app.get('/', (req, res) => {
             <p>GENERAL_TIMEOUT : for event streams in seconds ( default : 15 )</p>
             <p>ENGINE_THREADS : engine Threads option ( default : 1 )</p>
             <p>ENGINE_MOVE_OVERHEAD : engine Move Overhead option in milliseconds ( default : 500 )</p>
+            <p>ALLOW_PONDER : set it to "true" to make the bot think on opponent time</p>
+            <p>USE_BOOK : set it to "true" to use lichess opening explorer book</p>
+            <p>BOOK_DEPTH : up to how plies should the bot use the book ( default : 20 )</p>
+            <p>BOOK_SPREAD : select the move from that many of the top book moves ( default : 4 )</p>
+            <p>BOOK_RATINGS : comma separated list of allowed book rating brackets ( default : "2200,2500")</p>
+            <p>BOOK_SPEEDS : comma separated list of allowed book speeds ( default : "blitz,rapid" )</p>
+            <p>LOG_API : set it to "true" to allow more verbose logging</p>
             <script>            
             function processSource(blob){
                 if(blob.kind == "tick"){                    
