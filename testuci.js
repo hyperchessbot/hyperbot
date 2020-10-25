80785bcc5a608adac78101878b8c7641c4e517f6
hyperchessbot <hyperchessbot@gmail.com>
2020-10-25 11:52:14 +0100

    Drop

--- testuci.js
+++ testuci.js
@@ -1,24 +1,35 @@
-const path = require('path')
+if(false){
+	const path = require('path')
 
-const UciEngine = require('@easychessanimations/uci')
+	const UciEngine = require('@easychessanimations/uci')
 
-const engine = new UciEngine(path.join(__dirname, 'stockfish12'))
+	const engine = new UciEngine(path.join(__dirname, 'stockfish12'))
 
-engine.logProcessLine = false
+	engine.logProcessLine = false
 
-engine.position("startpos", ["e2e4", "c7c5"]).go({wtime: 3000, winc: 0, btime: 3000, binc: 0, ponder: true})
+	engine.position("startpos", ["e2e4", "c7c5"]).go({wtime: 3000, winc: 0, btime: 3000, binc: 0, ponder: true})
 
-console.log(engine.pondering)
+	console.log(engine.pondering)
 
-setTimeout(_=>{
-    /*console.log("stopping engine")
-    engine.stop(_=>{
-        console.log("stopped", engine.info)
-        engine.quit()
-    })*/
+	setTimeout(_=>{
+	    /*console.log("stopping engine")
+	    engine.stop(_=>{
+	        console.log("stopped", engine.info)
+	        engine.quit()
+	    })*/
 
-    engine.position("startpos", ["e2e4", Math.random()>0.5?"c7c6":"c7c5"]).gothen({wtime: 3000, winc: 0, btime: 3000, binc: 0}).then(result=>{
-        console.log(result.bestmove)
-        engine.quit()
-    })
-}, 3000)
+	    engine.position("startpos", ["e2e4", Math.random()>0.5?"c7c6":"c7c5"]).gothen({wtime: 3000, winc: 0, btime: 3000, binc: 0}).then(result=>{
+	        console.log(result.bestmove)
+	        engine.quit()
+	    })
+	}, 3000)	
+}
+
+const { Scalachess } = require('@easychessanimations/scalachess');
+
+(async function(){
+	let sc = new Scalachess('crazyhouse')
+	console.log(await sc.init())
+	console.log(await sc.makeMoves(['e2e4','d7d5','e4d5','e7e6','P@g4']))
+})()
+
