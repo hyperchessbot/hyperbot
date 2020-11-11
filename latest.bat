b8c40a757d5aea3300568a813db2d4680791819b
hyperchessbot <hyperchessbot@gmail.com>
2020-11-11 22:45:39 +0000

    Latest

--- latest.bat
+++ latest.bat
@@ -1,5 +1,6 @@
 move serve.sh serve.sh.old
 move serve.bat serve.bat.old
+mv lc0goorm/weights.pb.gz lc0goorm/weights.pb.gz.old
 
 git remote add upstream https://github.com/hyperchessbot/hyperbot.git
 
@@ -7,3 +8,4 @@ git pull upstream master
 
 move serve.sh.old serve.sh
 move serve.bat.old serve.bat
+mv lc0goorm/weights.pb.gz.old lc0goorm/weights.pb.gz
--- latest.sh
+++ latest.sh
@@ -1,5 +1,6 @@
 mv serve.sh serve.sh.old
 mv serve.bat serve.bat.old
+mv lc0goorm/weights.pb.gz lc0goorm/weights.pb.gz.old
 
 git remote add upstream https://github.com/hyperchessbot/hyperbot.git
 
@@ -7,3 +8,4 @@ git pull upstream master
 
 mv serve.sh.old serve.sh
 mv serve.bat.old serve.bat
+mv lc0goorm/weights.pb.gz.old lc0goorm/weights.pb.gz
