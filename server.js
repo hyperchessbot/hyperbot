260f7fd4b0fc7ab25d04e938c0d8e0a309c483bf
easychessanimations <chessbotroulette@gmail.com>
2020-11-12 10:57:39 +0000

    Score in pawns

--- server.js
+++ server.js
@@ -372,7 +372,7 @@ For detailed instructions see <a href="https://lichess.org/forum/off-topic-discu
 
                     if(content.match(/^bestmove/)){
 						let m = content.match(/score value: (.*)/)
-						let scoreValue = parseInt(m[1])
+						let scoreValue = parseInt(m[1])/100
 						let color = scoreValue >= 0 ? "#070" : "#700"
 						let scoreUnit = content.match(/score unit: ([^,]+)/)[1]
 						content = content.replace(m[0], \`score value: <span style="font-size: 20px;color: \${color};font-weight: bold;">\${scoreUnit == "mate" ? "#":""}\${scoreValue}</span>\`)
