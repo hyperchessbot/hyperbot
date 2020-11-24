47bc675e614ce087edb183bbc740c06814b6c1f0
hyperchessbot <hyperchessbot@gmail.com>
2020-11-24 07:25:27 +0000

    Use cors

--- server.js
+++ server.js
@@ -154,6 +154,10 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+const cors = require('cors')
+
+app.use(cors())
+
 const fetch = require('node-fetch')
 
 let docs = null
